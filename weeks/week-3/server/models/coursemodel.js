e6988d7d8f008824a4beddf8ad511b1ea580f0b3
koala31BM <kenneger31@gmail.com>
2026-02-18 21:43:27 -0600

    weeks3

---
+++ weeks/week-3/server/models/coursemodel.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  credits: {
+    type: Number,
+    required: true
+  }
+});
+
+module.exports = mongoose.model('Course', courseSchema);
