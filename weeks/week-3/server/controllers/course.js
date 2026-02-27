3d41ffe2a553be3f50f679106f151a25d9576d3b
koala31BM <kenneger31@gmail.com>
2026-02-26 20:03:06 -0600

    acomodo de weeks

--- weeks/week-3/server/controllers/course.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const courseSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  credits: Number
-});
-
-module.exports = mongoose.model('Course', courseSchema);
